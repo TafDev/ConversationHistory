51d2906ace0e90dcfc73e8be8d149ddbcad33316
TafDev <chombot@hotmail.com>
2023-05-18 17:23:36 +0100

    [feature] - Add the view for the show page

---
+++ spec/views/projects/show.html.erb_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'projects/show', type: :view do
+  let(:project) { create(:project) }
+
+  before do
+    assign(:project, project)
+    assign(:comment, Comment.new)
+    assign(:project_histories, project.project_histories.order(created_at: :desc))
+    render
+  end
+
+  it 'renders the project details', :aggregate_failures do
+    expect(rendered).to have_content(project.title)
+    expect(rendered).to have_content(project.status)
+  end
+
+  it 'renders the edit status form', :aggregate_failures do
+    expect(rendered).to have_select('project_status')
+    expect(rendered).to have_button('Update Status')
+  end
+
+  it 'renders the add comment form', :aggregate_failures do
+    expect(rendered).to have_field('comment_username')
+    expect(rendered).to have_field('comment_body')
+    expect(rendered).to have_button('Add Comment')
+  end
+
+  it 'renders the project history', :aggregate_failures do
+    expect(rendered).to have_content('Project History')
+
+    project.project_histories.each do |history|
+      expect(rendered).to have_content(history.event_type)
+      expect(rendered).to have_content(history.event_body)
+      expect(rendered).to have_content(history.created_at)
+    end
+  end
+end
