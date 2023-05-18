7c1602fae5316e8434bfb86ebbde9e9d8678c70e
TafDev <chombot@hotmail.com>
2023-05-18 12:47:02 +0100

    [bug] - Find project first

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,16 @@
 class CommentsController < ApplicationController
   def create
-    if @project.nil?
+    if project.nil?
       redirect_to projects_path, alert: t('projects.project_not_found')
       return
     end
 
-    @comment = @project.comments.build(comment_params)
+    @comment = project.comments.build(comment_params)
 
     if @comment.save
-      redirect_to @project, notice: t('comments.comment_added')
+      redirect_to project, notice: t('comments.comment_added')
     else
-      redirect_to @project, alert: t('comments.comment_failed')
+      redirect_to project, alert: t('comments.comment_failed')
     end
   end
 
@@ -18,5 +18,9 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:body, :username)
+  end
+
+  def project
+    @project ||= Project.find_by(id: params[:project_id])
   end
 end
