deeaafc91e32a3be5060d018188e99c1a3cd4d5c
TafDev <chombot@hotmail.com>
2023-05-18 01:35:08 +0100

    [refactor] - refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   get 'comments/create'
   resources :projects, only: %i[index show] do
     resources :comments, only: :create
-    patch 'change_status', to: 'projects#change_status'
   end
 end
