8356e549000cf6eb0ada5b3ae771c2a8530bbd28
TafDev <chombot@hotmail.com>
2023-05-18 15:35:44 +0100

    [feature] - Add project index view

---
+++ spec/views/projects/index.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'projects/index', type: :view do
+  before do
+    assign(:projects, [
+             create(:project, title: 'Project 1', status: 'in_progress'),
+             create(:project, title: 'Project 2', status: 'completed')
+           ])
+  end
+
+  it 'displays the list of projects', :aggregate_failures do
+    render
+
+    expect(rendered).to have_selector('h1', text: 'Projects')
+
+    expect(rendered).to have_selector('table.table.table-striped') do |table|
+      expect(table).to have_selector('thead') do |thead|
+        expect(thead).to have_selector('th', text: 'Title')
+        expect(thead).to have_selector('th', text: 'Status')
+      end
+
+      expect(table).to have_selector('tbody') do |tbody|
+        expect(tbody).to have_selector('tr', count: 2)
+      end
+    end
+  end
+end
